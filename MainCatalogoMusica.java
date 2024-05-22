6f1d6f0510701fd5c4cc72467ec238b376b561c5
ITzKani <daganjv.pro@gmail.com>
2024-05-21 18:24:26 -0600

    Commit CatalogoMusica v0.7

--- MainCatalogoMusica.java
+++ MainCatalogoMusica.java
@@ -56,7 +56,7 @@ public class MainCatalogoMusica {
                         System.out.println("Genero: " + Lista.get(in).getGenero());
                         System.out.println("-------------------------------");
                     }
-                    
+
                     System.out.print("Quieres agregar otra cancion y/n: ");
                     opt = fn2.nextLine();
 
@@ -68,50 +68,46 @@ public class MainCatalogoMusica {
             case 2:
                 int idsearch;
                 do {
-                    
+
                     System.out.println("Consultorio de canciones");
                     System.out.println("-------------------------------");
                     System.out.println("*Escribe la ID de la cancion que deseas buscar*");
                     idsearch = fn2.nextInt();
-                    
-                   // if (!(idsearch < 0 && idsearch < Lista.size() + 1)) {
-                        System.out.println("-------------------------------");
-                        System.out.println("ID: " + Lista.get(idsearch - 1).getId());
-                        System.out.println("Cancion: " + Lista.get(idsearch - 1).getNombreCan());
-                        System.out.println("Artista: " + Lista.get(idsearch - 1).getArtista());
-                        System.out.println("Genero: " + Lista.get(idsearch - 1).getGenero());
-                        System.out.println("-------------------------------");
-                        
-                        System.out.print("Buscar otra cancion y/n: ");
-                        fn2.nextLine();
-                        opt2 = fn2.nextLine();
-                    //}
 
-                   // else {
-                       // System.out.println("*Error: Tienes que ingresar un numero.*");
-                    //}
+                    System.out.println("-------------------------------");
+                    System.out.println("ID: " + Lista.get(idsearch - 1).getId());
+                    System.out.println("Cancion: " + Lista.get(idsearch - 1).getNombreCan());
+                    System.out.println("Artista: " + Lista.get(idsearch - 1).getArtista());
+                    System.out.println("Genero: " + Lista.get(idsearch - 1).getGenero());
+                    System.out.println("-------------------------------");
+
+                    System.out.print("Buscar otra cancion y/n: ");
+                    fn2.nextLine();
+                    opt2 = fn2.nextLine();
+
                     System.out.println(opt2);
                 } while (!(opt2.equals("n")));
                 break;
-                
+
             case 3:
-                
-                mlp.mostrarCanciones();
+
+                mlp.mostrarCatalogo();
                 break;
-                
+
             default:
                 break;
         }
     }
 
-    public void mostrarCanciones(){
-        do{
-        System.out.println("-------------------------------");
-        System.out.println("Catalogo de canciones");
-        System.out.print("Salir del catalogo y/n: ");
-        opt = fn2.nextLine();
-        }while((opt.equals("n")));
-    
-        
+    // * Metodo de mostrar catalogo (opcion 3 del menu)
+
+    public void mostrarCatalogo() {
+        do {
+            System.out.println("-------------------------------");
+            System.out.println("Catalogo de canciones");
+            System.out.print("Salir del catalogo y/n: ");
+            opt = fn2.nextLine();
+        } while ((opt.equals("n")));
+
     }
 }
